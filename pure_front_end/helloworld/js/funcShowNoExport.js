fc1e38af6aba8aaf369c5af6c89b660e367d3aad
oak42 <oakch@163.com>
2019-06-16 06:52:48 +0800

    webpack

--- pure_front_end/helloworld/js/funcShowNoExport.js
+++ pure_front_end/helloworld/js/funcShowNoExport.js
@@ -1,9 +1,9 @@
-export default {
-  show(content) {
-    window.document.getElementById('application7').innerText = `hello, ${content}`;
-    func1(1);
-  },
-};
+show('ackerley without using webpack...');
+
+function show(content) {
+  window.document.getElementById('application7').innerText = `hello, ${content}`;
+  func1(1);
+}
 
 function func1(param) {
   console.log('func1 entered');
